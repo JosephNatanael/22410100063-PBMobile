7b6ffcc2923287c8d1a63b8c5129e687ea0f36a6
Joseph Natanael <146148737+JosephNatanael@users.noreply.github.com>
2024-04-04 22:54:19 +0700

    Latihan02
    Tugas pert 3

---
+++ tugas03.dart
@@ -0,0 +1,11 @@
+class Persegi{
+  int p =20;
+  static int l = 30;
+  static Luas(int p1, int l1){
+    return (p1*l1);
+  }
+}
+void main(){
+  print(Persegi.l);
+  print(Persegi.Luas(2,3).toString());
+}
